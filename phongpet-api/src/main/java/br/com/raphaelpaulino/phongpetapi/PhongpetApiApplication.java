31d53f1ff69984e8646c48316c202eacd08cda90
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-04-17 19:35:50 -0300

    first commit

---
+++ phongpet-api/src/main/java/br/com/raphaelpaulino/phongpetapi/PhongpetApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.raphaelpaulino.phongpetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhongpetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhongpetApiApplication.class, args);
+	}
+
+}
---
+++ phongpet-api/src/test/java/br/com/raphaelpaulino/phongpetapi/PhongpetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raphaelpaulino.phongpetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhongpetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
