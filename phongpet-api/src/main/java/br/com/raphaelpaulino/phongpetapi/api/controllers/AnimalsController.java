b39518e9a73df7e9dc66430104b70e6d72e2a7f3
Raphael Paulino <raphael.paulino.web@gmail.com>
2021-04-27 20:39:02 -0300

    - methods findById(), store(), update() and destroy() created

--- phongpet-api/src/main/java/br/com/raphaelpaulino/phongpetapi/api/controllers/AnimalsController.java
+++ phongpet-api/src/main/java/br/com/raphaelpaulino/phongpetapi/api/controllers/AnimalsController.java
@@ -2,16 +2,14 @@ package br.com.raphaelpaulino.phongpetapi.api.controllers;
 
 import br.com.raphaelpaulino.phongpetapi.domain.repository.AnimalRepository;
 import br.com.raphaelpaulino.phongpetapi.models.Animal;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// @Controller
 @RestController
 @RequestMapping("/animals")
 public class AnimalsController {
@@ -22,5 +20,42 @@ public class AnimalsController {
     @GetMapping
     public List<Animal> all() {
         return animalRepository.all();
+    }
+
+    @GetMapping("/{animalId}")
+    public ResponseEntity<Animal> findById(@PathVariable Long animalId) {
+        Animal animal = animalRepository.byId(animalId);
+        return ResponseEntity.ok(animal);
+    }
+
+    @PostMapping
+    public ResponseEntity<Animal> store(@RequestBody Animal animal) {
+        animal = animalRepository.add(animal);
+        return ResponseEntity.status(HttpStatus.CREATED).body(animal);
+    }
+
+    @PutMapping("/{animalId}")
+    public ResponseEntity<Animal> update(@PathVariable Long animalId, @RequestBody Animal animal) {
+        Animal currentAnimal = animalRepository.byId(animalId);
+
+        if (currentAnimal != null) {
+            BeanUtils.copyProperties(animal, currentAnimal, "id");
+            animalRepository.add(currentAnimal);
+            return ResponseEntity.ok(currentAnimal);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{animalId}")
+    public ResponseEntity<?> destroy(@PathVariable Long animalId) {
+        Animal animal = animalRepository.byId(animalId);
+
+        if (animal != null) {
+            animalRepository.remove(animal);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
